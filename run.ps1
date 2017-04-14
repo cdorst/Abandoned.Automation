41ffb4928c41ac5dcc1b4ee91b9b681e01c6f63f
Chris Dorst <christopherd@health.ok.gov>
2017-04-14 00:01:49 -0500

    Modifying build script. Running dotnet commands for the preparation project instead of at the solution level

--- run.ps1
+++ run.ps1
@@ -1,6 +1,9 @@
-& dotnet restore
-& dotnet build
 & cd Preparation.ConsoleApp
+Write-Host "Restoring Packages"
+& dotnet restore
+Write-Host "Builing preparation app"
+& dotnet build
+Write-Host "Running preparation app"
 & dotnet run -t "$PSScriptRoot/Preparation.ConsoleApp/template.json" -o "$PSScriptRoot/Automation.ConsoleApp/Services/AutomationService.cs" -d "$PSScriptRoot"
 & cd ..
 & dotnet restore
